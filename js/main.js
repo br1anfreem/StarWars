dee35e6a97eaca4e9f6919b5742b6f0efa02dd23
Andrey <60319712+br1anfreem@users.noreply.github.com>
2020-04-17 11:53:05 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        navClass:['slider__nav--left','slider__nav--right'],
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            700:{
+                items:2
+            },
+            900:{
+                items:3
+            },
+            1200:{
+                items:4
+            }
+        }
+    })
+});
